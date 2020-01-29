8f05793cad1efb5d88bb56967be94732cf1aeccb
mi-blog <lmezach@gmail.com>
2020-01-29 14:39:39 -0430

    themas lte

---
+++ resources/views/theme/lte/layout.balde.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>AdminLTE 3 | Boxed Layout</title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Font Awesome -->       
+        <link rel="stylesheet" href='{{asset("assets/$theme/plugins/fontawesome-free/css/all.min.css")}}'>
+        
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        
+        <!-- Theme style -->
+        <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+        <link rel="stylesheet" href='{{asset("assets/$theme/plugins/dist/css/adminlte.min.css")}}'>
+        <!-- Google Font: Source Sans Pro -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+    <body class="hold-transition sidebar-mini layout-boxed">
+    <!-- Site wrapper -->
+        <div class="wrapper">
+
+        <!--inicio heder-->
+        @include("theme/$theme/header")
+        <!--fin heder-->
+        <!--inici aside-->
+        @include("theme/$theme/aside")
+        <!--fin aside-->
+
+
+
+
+
+
+
+       <!--inici footer-->
+       @include("theme/$theme/footer")
+        <!--fin footer-->
+
+        </div>
+
+        <!-- jQuery -->
+        <script src='{{asset("assets/$theme/plugins/jquery/jquery.min.js")}}'></script>
+        <!-- Bootstrap 4 -->
+        <script src='{{asset("assets/$theme/plugins/bootstrap/js/bootstrap.bundle.min.js"}}'></script>
+        <!-- AdminLTE App -->
+        <script src='{{asset("assets/$theme/dist/js/adminlte.min.js"}}'></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src='{{asset("assets/$theme/dist/js/demo.js")}}'></script>
+    </body>
+</html>
