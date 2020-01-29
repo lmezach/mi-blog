94198d080b046d9b2e7b436a1d2b0c60fc1aedab
mi-blog <lmezach@gmail.com>
2020-01-29 15:53:39 -0430

    themas mas

--- resources/views/theme/lte/aside.blade.php
+++ resources/views/theme/lte/aside.blade.php
@@ -1,12 +1,12 @@
  <!-- Main Sidebar Container -->
- <aside class="main-sidebar sidebar-dark-primary elevation-4">
+ <aside class="main-sidebar elevation-4 sidebar-light-primary">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
-      <img src="../../dist/img/AdminLTELogo.png"
+    <a href="/" class="brand-link">
+      <img src='{{asset("assets/$theme/dist/img/UNEFA.png")}}'
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">UNEFA</span>
     </a>
 
     <!-- Sidebar -->
